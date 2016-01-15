2f0731c04820385867775bd3d0ebec077a8e490c
Andy White <andywhite@quillcontent.com>
2016-01-15 09:06:50 +0000

    Better specs

--- spec/jobs/vote_mailer_job_spec.rb
+++ spec/jobs/vote_mailer_job_spec.rb
@@ -1,29 +1,36 @@
 require "rails_helper"
 
 RSpec.describe VoteMailerJob do
-  let(:voter) { double("Voter") }
-  let(:movie) { double("Movie") }
-  let(:fake_mail) { double("FakeMail", deliver: nil) }
+  let(:voter) { double("Voter", name: "Mr Voter") }
+  let(:movie) { double("Movie", user: fake_user, title: "Grease") }
+  let(:fake_mail) { double("Mail", deliver: nil) }
+  let(:fake_user) { double("User", name: "Mr Fake", email: "fake@user.com") }
 
   context "Type is :like" do
-    before do
-      allow(VoteMailer).to receive(:like_email) { fake_mail }
-    end
-
     it "Calls VoteMailer.like_email" do
+      allow(VoteMailer).to receive(:like_email) { fake_mail }
       expect(VoteMailer).to receive(:like_email).once
       subject.perform(:like, voter, movie)
+    end
+
+    it "Sends the email" do
+      expect {
+        subject.perform(:like, voter, movie)
+      }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 
   context "Type is :hate" do
-    before do
-      allow(VoteMailer).to receive(:hate_email) { fake_mail }
-    end
-
     it "Calls VoteMailer.hate_email" do
+      allow(VoteMailer).to receive(:hate_email) { fake_mail }
       expect(VoteMailer).to receive(:hate_email).once
       subject.perform(:hate, voter, movie)
+    end
+
+    it "Sends the email" do
+      expect {
+        subject.perform(:hate, voter, movie)
+      }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 
---
+++ spec/views/vote_mailer/hate_email.haml_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "vote_mailer/hate_email.html.haml", :type => :view do
+  let(:fake_user) { double("User", name: "Jon Jaws") }
+
+  it "Displays correct message" do
+    assign(:movie, double("Movie", user: fake_user, title: "Jaws"))
+    assign(:voter, double("Voter", name: "Jaws Liker"))
+
+    render
+
+    expect(response.body).to match /Dear\nJon Jaws/m
+    expect(response.body).to match /Jaws Liker\nhates your movie:\nJaws/
+  end
+end
